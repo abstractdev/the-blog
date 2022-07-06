2def477e867ecd197d40202f3a2bd14abff488e0
Andrew R <abstractdev0@gmail.com>
2022-07-06 12:00:50 -0400

    feat: Create Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,18 @@
 import styles from "./Footer.module.scss";
+import github from "../../assets/images/GitHub-Mark-32px.png";
 
 function Footer() {
   return (
-    <>
-      <div>Footer</div>;
-    </>
+    <footer className={styles.footer}>
+      <a
+        href="https://github.com/abstractdev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={github} alt="github icon" />
+        abstractdev
+      </a>
+    </footer>
   );
 }
 
