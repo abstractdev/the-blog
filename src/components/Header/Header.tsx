daae6216ca972d35d48931425618338d2ba03eaf
Andrew R <abstractdev0@gmail.com>
2022-07-06 12:07:27 -0400

    feat: Create header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,24 @@
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
+import { PropsInterface } from "../../interfaces/PropsInterface";
 
-function Header() {
+function Header(props: PropsInterface) {
+  const { setCategorizedBlogData } = props;
+
   return (
-    <>
-      <div>Header</div>;
-    </>
+    <header className={styles.header}>
+      <Link to="/" onClick={() => setCategorizedBlogData!(null)}>
+        <h1 className={styles["blog-name"]}>The Blog</h1>
+      </Link>
+      <nav className={styles.nav}>
+        <Link to="/login" className={styles["login-link"]}>
+          Log In
+        </Link>
+        <Link to="/signup" className={styles["signup-link"]}>
+          Sign Up
+        </Link>
+      </nav>
+    </header>
   );
 }
 
