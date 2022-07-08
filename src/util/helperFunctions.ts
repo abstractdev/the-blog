4d2e0ed105c897cff7909cd8042a87004123800f
Andrew R <abstractdev0@gmail.com>
2022-07-07 22:56:13 -0400

    chore: Add formatTitle function

--- src/util/helperFunctions.ts
+++ src/util/helperFunctions.ts
@@ -1,9 +1,13 @@
-export function formatDate(isoString: string) {
-  const msSinceEpoch = Date.parse(isoString);
+export function formatDate(isoString: string | undefined) {
+  const msSinceEpoch = Date.parse(isoString!);
   return new Date(msSinceEpoch).toLocaleString("en-US", {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   });
+}
+
+export function formatTitle(string: string | undefined) {
+  return string!.replace(/-/g, " ");
 }
