ec0f2ca87b4162734aa0f43d43a6f866d676a8c6
Andrew R <abstractdev0@gmail.com>
2022-07-10 20:45:24 -0400

    chore: Add date function

--- src/util/helperFunctions.ts
+++ src/util/helperFunctions.ts
@@ -7,6 +7,14 @@ export function formatDate(isoString: string | undefined) {
     day: "numeric",
   });
 }
+export function formatShortDate(isoString: string | undefined) {
+  const msSinceEpoch = Date.parse(isoString!);
+  return new Date(msSinceEpoch).toLocaleDateString("en-US", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+  });
+}
 
 export function formatTitle(string: string | undefined) {
   return string!.replace(/-/g, " ");
