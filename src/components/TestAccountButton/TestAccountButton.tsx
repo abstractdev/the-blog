e8fa2d3c11f564feb210c27e09f349e1c040d4dc
Andrew R <abstractdev0@gmail.com>
2022-07-16 23:20:31 -0400

    feat: Create TestAccountButton component

---
+++ src/components/TestAccountButton/TestAccountButton.tsx
@@ -0,0 +1,43 @@
+import styles from "./TestAccountButton.module.scss";
+import { useNavigate } from "react-router-dom";
+import { PropsInterface } from "../../interfaces/PropsInterface";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
+import { Icon } from "@iconify/react";
+
+function TestAccountButton(props: PropsInterface) {
+  const { buttonName } = props;
+  const { user, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  async function handleLoginClick() {
+    let loginData = { username: "", password: "" };
+    if (buttonName === "Test Author") {
+      loginData.username = "TestAuthor";
+      loginData.password = "testauthor";
+    } else if (buttonName === "Test User") {
+      loginData.username = "TestUser";
+      loginData.password = "testuser";
+    }
+    await fetch("https://a-blog-api.herokuapp.com/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(loginData),
+    });
+    setUser!({ ...user, isLoggedIn: true });
+    navigate("/profile");
+  }
+
+  return (
+    <button
+      className={styles["test-account-button"]}
+      onClick={() => handleLoginClick()}
+    >
+      <Icon className={styles.icon} icon="healthicons:ui-user-profile" />
+      {buttonName}
+    </button>
+  );
+}
+
+export default TestAccountButton;
