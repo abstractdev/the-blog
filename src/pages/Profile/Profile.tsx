818f98c5ca75804d83b68d7dce862e34f02705cf
Andrew R <abstractdev0@gmail.com>
2022-07-16 23:24:35 -0400

    feat: Create Profile page

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,23 @@
+import { useContext, useEffect } from "react";
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import { AuthContext } from "../../contexts/AuthContext";
+
+function Profile() {
+  const { user, setUser } = useContext(AuthContext);
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://a-blog-api.herokuapp.com/users", {
+        method: "GET",
+        credentials: "include",
+      });
+      const resData = await res.json();
+      if (resData) {
+        setUser!({ ...resData, isLoggedIn: true });
+      }
+    })();
+  }, []);
+
+  return <>{user?.isLoggedIn && <ProfileCard />}</>;
+}
+
+export default Profile;
