fa0950ace5f1370ed7183fdd0b80d117dcd7735e
Andrew R <abstractdev0@gmail.com>
2022-07-16 23:24:53 -0400

    chore: Create routes

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./components/App/App";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
+import UserSignup from "./pages/UserSignup/UserSignup";
+import Blogpost from "./pages/Blogpost/Blogpost";
+import Profile from "./pages/Profile/Profile";
+import AuthorSignup from "./pages/AuthorSignup/AuthorSignup";
+import AuthorCreate from "./pages/AuthorCreate/AuthorCreate";
+import AuthorView from "./pages/AuthorView/AuthorView";
+import AuthorEdit from "./pages/AuthorEdit/AuthorEdit";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,7 +23,13 @@ root.render(
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup/user" element={<UserSignup />} />
+          <Route path="/signup/author" element={<AuthorSignup />} />
+          <Route path="/:blogpostTitle" element={<Blogpost />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/author/create" element={<AuthorCreate />} />
+          <Route path="/author/edit/:blogpostTitle/:blogpostId" element={<AuthorEdit />} />
+          <Route path="/author/view" element={<AuthorView />} />
         </Route>
       </Routes>
     </BrowserRouter>
