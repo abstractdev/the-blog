5a9dfe9947e8ffee476a99afc00e21fa89a4ffd3
Andrew R <abstractdev0@gmail.com>
2022-07-16 23:24:29 -0400

    feat: Create Blogpost page

---
+++ src/pages/Blogpost/Blogpost.tsx
@@ -0,0 +1,42 @@
+import { useParams } from "react-router-dom";
+import { BlogDataInterface } from "../../interfaces/BlogData";
+import { useEffect, useState } from "react";
+import BlogpostCard from "../../components/BlogpostCard/BlogpostCard";
+import CommentFormCard from "../../components/CommentFormCard/CommentFormCard";
+import Comments from "../../components/Comments/Comments";
+import { CommentsArrayType } from "../../interfaces/Comments";
+
+function Blogpost() {
+  const [comments, setComments] = useState<CommentsArrayType | null>(null);
+  const [blogpostId, setBlogpostId] = useState("");
+  const [blogpostData, setBlogpostData] = useState<BlogDataInterface | null>(
+    null
+  );
+  const { blogpostTitle } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(
+        `https://a-blog-api.herokuapp.com/blog/${blogpostTitle}`
+      );
+      const resData = await res.json();
+      setBlogpostData(resData);
+      setBlogpostId(resData.id);
+      setComments(resData.comments);
+    })();
+  }, []);
+
+  return (
+    <>
+      <BlogpostCard blogpostData={blogpostData} blogpostTitle={blogpostTitle} />
+      <Comments comments={comments} blogpostId={blogpostId} />
+      <CommentFormCard
+        blogpostId={blogpostId}
+        comments={comments}
+        setComments={setComments}
+      />
+    </>
+  );
+}
+
+export default Blogpost;
